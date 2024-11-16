aa4fed83722a0d65be736f5293cdac653591a1f9
Zeeshan Siddiqui <zeeshansd767@gmail.com>
2024-11-16 15:10:39 +0500

    commit changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,18 +13,11 @@ const Navbar = () => {
         <nav className="navbar flex justify-between items-center p-4 shadow-md">
             <div className='main-name flex items-center gap-2'>
                 <FaShopify color='red' size='30' />
-                <h1 className="text-xl font-bold">Dashboard</h1>
+                <h1 className="text-xl font-bold">Best Store</h1>
             </div>
             <div className='nav-links flex gap-4'>
 
-                <NavLink 
-                    to='/' 
-                    className={({ isActive }) =>
-                        isActive ? 'text-[#f97070] underline font-bold nav-link' : 'text-black font-bold nav-link'
-                    }
-                >
-                    Home
-                </NavLink>
+             
                 {/* Add more links as needed */}
             </div>
             <div className='flex items-center gap-4'>
@@ -49,6 +42,14 @@ const Navbar = () => {
                         </button>
                     </div>
                 ) : ( */}
+                   <NavLink 
+                    to='/' 
+                    className={({ isActive }) =>
+                        isActive ? 'text-[#f97070] underline font-bold nav-link' : 'text-black font-bold nav-link'
+                    }
+                >
+                    Home
+                </NavLink>
                     <Link to="/account" className='login-button text-blue-500 font-semibold'>
                         <b>Log in/Sign Up</b>
                     </Link>
--- src/Screens/Index.jsx
+++ src/Screens/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Index = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Wellcome to our website</h1>
     </div>
   )
 }
